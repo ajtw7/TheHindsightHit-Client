e5efbec586dd82b9a0b93f3e1cb5ef5564834162
ajtw7 <andrew.worsley91@gmail.com>
2024-01-16 01:16:01 -0500

    installed brute force api via useEffect to get data on frontend. has no structure at moment.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import fetch from 'node-fetch';
 import './App.css';
 
 function App() {
+  // const [mgrData, setMgrData] = useState([]);
+  const [fixtures, setFixtures] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/api/mgr-profile')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       // console.log('data', data);
+  //       setMgrData(data);
+  //     });
+  // });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/api/fixtures');
+        const data = await res.json();
+        setFixtures((prevFixtures) => {
+          return data;
+        });
+        console.log('these are the fixtures', fixtures);
+      } catch (error) {
+        console.error('Error fetching fixtures', error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>THE HINDSIGHT HIT</h1>
+      <div
+        style={{
+          width: '100%',
+          height: '10vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {/* <>{mgrData.id}</>
+        <>{mgrData.name}</> */}
+      </div>
+      <div>
+        {fixtures.map((fix) => (
+          <div key={fix.id}>
+            <p>Event: {fix.event}</p>
+            <p>Pulse Id: {fix.pulse_id}</p>
+            <p>Team: {fix.team_a}</p>
+            <br />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
