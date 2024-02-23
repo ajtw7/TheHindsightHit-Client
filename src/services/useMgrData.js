d37d36c100f3ee90341186c872ba27b3cc6fa789
ajtw7 <andrew.worsley91@gmail.com>
2024-02-22 20:24:26 -0500

    Similar to prev commit: made slight edit (insert underscore pre) to arg/copy being passed in the setter func. for FETCH. Imported (or re-imported) useEffect/useState for services. Also, updated path at the bottom of page.

--- src/services/useMgrData.js
+++ src/services/useMgrData.js
@@ -1,22 +1,22 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 export default function useMgrData() {
-    const [mgrData, setMgrData] = useState([]);
-    
-    useEffect(() => {
-        const fetchData = async () => {
-        try {
-            const res = await fetch('http://localhost:5000/api/mgr-profile');
-            const data = await res.json();
-            setMgrData((prevMgrData) => {
-            return data;
-            });
-        } catch (error) {
-            console.error('Error fetching manager data', error);
-        }
-        };
-        fetchData();
-    }, []);
-    
-    return mgrData;
-    }
+  const [mgrData, setMgrData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/api/mgr-profile');
+        const data = await res.json();
+        setMgrData((_prevMgrData) => {
+          return data;
+        });
+      } catch (error) {
+        console.error('Error fetching manager data', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return mgrData;
+}
