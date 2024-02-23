0d132fa041bf5e91cc235f94638fe9a92cb059b6
ajtw7 <andrew.worsley91@gmail.com>
2024-02-22 23:03:11 -0500

    Imported  the following services: useAllPlayers, useMgrData, useGWPlayerStats, usePlayerHistories. Imported the new ManagerProfile and Fixtures components. Initialiazed the PlayerContext. Deconstructed the following variables from their respective hooks: mgrData, allPlayers, myPlayerStats. Then used aforemention variables to initialize myPlayerIds, myPlayers, and playerHistories. All we passed to the value of PlayerContext.Provider. Removed fetch to aquire fixtures and replaced with Fixtures component.

--- src/App.js
+++ src/App.js
@@ -1,65 +1,75 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import fetch from 'node-fetch';
-import MgrProfile from './MgrProfile';
-import AllPlayers from './services/useAllPlayers';
+import React, { useMemo } from 'react';
+import useAllPlayers from './services/useAllPlayers';
+import useMgrData from './services/useMgrData';
+import useGWPlayerStats from './services/useGWPlayerStats';
+import usePlayerHistories from './services/usePlayerHistories';
+import ManagerProfile from './ManagerProfile';
 import GWHistory from './GWHistory';
-import PlayerStats from './services/usePlayerHistories';
+import Fixtures from './Fixtures';
 import './styles/App.css';
 
-function App() {
-  // const [mgrData, setMgrData] = useState([]);
-  const [fixtures, setFixtures] = useState([]);
+export const PlayerContext = React.createContext();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch('http://localhost:5000/api/fixtures');
-        const data = await res.json();
-        setFixtures((prevFixtures) => {
-          return data;
-        });
-        console.log('these are the fixtures', fixtures);
-      } catch (error) {
-        console.error('Error fetching fixtures', error);
-      }
-    };
-    fetchData();
-  }, []);
+function App() {
+  const mgrData = useMgrData();
+  const allPlayers = useAllPlayers();
+  const myPlayerStats = useGWPlayerStats();
+
+  const myPlayerIds = useMemo(() => {
+    return myPlayerStats ? myPlayerStats.map((player) => player.element) : [];
+  }, [myPlayerStats]);
+
+  const myPlayers = useMemo(() => {
+    return allPlayers.filter((player) => {
+      return myPlayerIds.includes(player.id);
+    });
+  }, [allPlayers, myPlayerIds]);
+
+  // Use the usePlayerHistories hook once for each player ID
+  const playerHistories = usePlayerHistories(myPlayerIds);
 
   return (
-    <Router>
-      <div className="App">
-        <h1>THE HINDSIGHT HIT</h1>
+    <PlayerContext.Provider
+      value={{ mgrData, myPlayerIds, myPlayers, playerHistories }}
+    >
+      <Router>
+        <div className="App">
+          <h1>THE HINDSIGHT HIT</h1>
 
-        <nav className="sticky-menu">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/manager-profile">Manager Profile</Link>
-            </li>
-            <li>
-              <Link to="/gameweek-history">GW History</Link>
-            </li>
-            <li>
-              <Link to="/all-players">All Players</Link>
-            </li>
-            <li>
-              <Link to="/player-stats">Player Stats</Link>
-            </li>
-          </ul>
-        </nav>
+          <nav className="sticky-menu">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/manager-profile">Manager Profile</Link>
+              </li>
+              <li>
+                <Link to="/gameweek-history">GW History</Link>
+              </li>
+              {/* <li>
+                <Link to="/all-players">All Players</Link>
+              </li> */}
+              <li>
+                <Link to="/player-stats">Player Stats</Link>
+              </li>
+              <li>
+                <Link to="/fixtures">Fixtures</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Routes>
-          <Route path="/manager-profile" element={<MgrProfile />} />
-          <Route path="/gameweek-history" element={<GWHistory />} />
-          <Route path="/all-players" element={<AllPlayers />} />
-          <Route path="/player-stats" element={<PlayerStats />} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/manager-profile" element={<ManagerProfile />} />
+            <Route path="/gameweek-history" element={<GWHistory />} />
+            {/* <Route path="/all-players" element={<useAllPlayers />} /> */}
+            <Route path="/player-stats" element={<useGWPlayerStats />} />
+            <Route path="/fixtures" element={<Fixtures />} />
+          </Routes>
+        </div>
+      </Router>
+    </PlayerContext.Provider>
   );
 }
 
