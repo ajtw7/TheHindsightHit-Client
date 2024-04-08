46868508a5f9ab41d55175b6b6ef70ba7c9fd0fe
ajtw7 <andrew.worsley91@gmail.com>
2024-04-08 13:26:59 -0400

    New Home Page component to allow Managers to enter their team ID. Meant to serve as a more of a 'fence' than 'gate.

---
+++ src/HomePage.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+export default function HomePage({ setMgrId }) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setMgrId(inputValue);
+    console.log('setMgrId:', setMgrId);
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+        textAlign: 'center',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          border: '1px solid black',
+          padding: 50,
+        }}
+      >
+        <h1>Welcome Manager!</h1>
+        <h3>please enter your team ID first to enter your LAB!</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Enter team ID"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
