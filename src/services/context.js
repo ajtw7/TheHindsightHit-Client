adc8dcb94dd1744cd2ad0b3cd39830eccce7d57c
ajtw7 <andrew.worsley91@gmail.com>
2024-04-08 01:28:16 -0400

    Init'd SelectedGWContext.

--- src/services/context.js
+++ src/services/context.js
@@ -2,4 +2,5 @@ import React from 'react';
 
 export const PlayerContext = React.createContext();
 export const ManagerContext = React.createContext();
-export const GWContext = React.createContext();
+export const SelectedGWContext = React.createContext();
+
