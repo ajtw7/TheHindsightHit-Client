ad4fdfe52699ee6de135e6522e5c9a6f67e685d9
ajtw7 <andrew.worsley91@gmail.com>
2024-04-12 23:31:13 -0400

    Added Datagrid to Transfers to display players in and out for each week. Also exported transfer columns for DG for clean code.

--- src/Transfers.js
+++ src/Transfers.js
@@ -1,17 +1,33 @@
 import { useContext } from 'react';
 import { PlayerContext } from './services/context';
+import { DataGrid } from '@mui/x-data-grid';
+import { transferColumnDef } from './transferColumnDef';
 
-export default function Transfers() {
-  const { myTransfers } = useContext(PlayerContext);
-  // const [transfers, setTransfers] = useState(myTransfers);
-  console.log('Transfers Log:', {
-    myTransfers,
-    // transfers
-  });
+export default function Transfers(myTransfers) {
+  const { allPlayers } = useContext(PlayerContext);
+  const myTransfersArray = Object.values(myTransfers);
+  const [transfers] = myTransfersArray;
+
+  const playerLookup = allPlayers.reduce((lookup, player) => {
+    lookup[player.id] = player.web_name;
+    return lookup;
+  }, {});
+
+  const namedTransfers = transfers.map((transfer) => ({
+    ...transfer,
+    element_in: playerLookup[transfer.element_in],
+    element_out: playerLookup[transfer.element_out],
+  }));
 
   return (
-    <div>
+    <div style={{ height: 'auto', width: '100%' }}>
       <h1>Transfers</h1>
+      <DataGrid
+        rows={namedTransfers}
+        columns={transferColumnDef}
+        pageSize={5}
+        getRowId={(row) => `${row.entry}-${row.event}-${row.time}`}
+      />
     </div>
   );
 }
---
+++ src/transferColumnDef.js
@@ -0,0 +1,60 @@
+export const transferColumnDef = [
+  {
+    field: 'element_in',
+    headerName: 'Player In',
+    width: 150,
+    flex: 1,
+    align: 'center',
+    headerAlign: 'center',
+  },
+  {
+    field: 'element_in_cost',
+    headerName: 'Player In Cost',
+    width: 150,
+    flex: 1,
+    align: 'center',
+    headerAlign: 'center',
+  },
+  {
+    field: 'element_out',
+    headerName: 'Player Out ',
+    width: 150,
+    flex: 1,
+    align: 'center',
+    headerAlign: 'center',
+  },
+  {
+    field: 'element_out_cost',
+    headerName: 'Player Out Cost',
+    width: 150,
+    flex: 1,
+    align: 'center',
+    headerAlign: 'center',
+  },
+
+  {
+    field: 'event',
+    headerName: 'GW',
+    width: 100,
+    flex: 1,
+    align: 'center',
+    headerAlign: 'center',
+  },
+  {
+    field: 'time',
+    headerName: 'Time',
+    width: 150,
+    flex: 1,
+    align: 'center',
+    headerAlign: 'center',
+  },
+
+  {
+    field: 'entry',
+    headerName: 'Entry',
+    width: 150,
+    flex: 1,
+    align: 'center',
+    headerAlign: 'center',
+  },
+];
