4c49d9d33eac4b4be563fa43253cc501cc108793
ajtw7 <andrew.worsley91@gmail.com>
2024-04-12 23:36:42 -0400

    Passing mgrId and setting loading stages for GET call

--- src/services/useTransfers.js
+++ src/services/useTransfers.js
@@ -1,21 +1,27 @@
 import { useState, useEffect } from 'react';
 
-export default function useTransfers() {
+export default function useTransfers(mgrId) {
   const [myTransfers, setMyTransfers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
-        const res = await fetch('http://localhost:5000/api/transfers/');
+        const res = await fetch(`http://localhost:5000/api/transfers/${mgrId}`);
         const data = await res.json();
         setMyTransfers((_prevTransfers) => {
           return data;
         });
       } catch (error) {
         console.error('Error fetching transfers', error);
+      } finally {
+        setLoading(false);
       }
     };
-    fetchData();
-  }, []);
-  return myTransfers;
+    if (mgrId) {
+      fetchData();
+    }
+  }, [mgrId]);
+  return { myTransfers, loading };
 }
