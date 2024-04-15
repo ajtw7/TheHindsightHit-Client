faa5eab2b24d4dd0e7dfeea1a099d3e7ff2b22d8
ajtw7 <andrew.worsley91@gmail.com>
2024-04-15 13:06:02 -0400

    Adding limiter to client-side GET to slow requests.

--- src/services/usePlayerHistories.js
+++ src/services/usePlayerHistories.js
@@ -2,15 +2,6 @@ import { useState, useEffect } from 'react';
 
 export default function usePlayerHistories(elementIds) {
   const [playerHistories, setPlayerHistories] = useState([]);
-  //   const {
-  //     element,
-  //     fixture,
-  //     total_points,
-  //     goals_scored,
-  //     assists,
-  //     clean_sheets,
-  //     bonus,
-  //   } = playerHistories;
 
   useEffect(() => {
     if (elementIds.length === 0) {
@@ -19,23 +10,29 @@ export default function usePlayerHistories(elementIds) {
     }
     const fetchData = async () => {
       try {
-        const histories = await Promise.all(
-          elementIds.map(async (id) => {
+        const limit = 10;
+        const queue = [...elementIds];
+        const results = [];
+
+        while (queue.length > 0) {
+          const promises = queue.splice(0, limit).map(async (id) => {
             const res = await fetch(
               `http://localhost:5000/api/player-history/${id}`
             );
             const data = await res.json();
             return data;
-          })
-        );
-        setPlayerHistories(histories);
+          });
+          results.push(...(await Promise.all(promises)));
+        }
+        setPlayerHistories(results);
       } catch (error) {
         console.error('Error fetching player history', error);
+        return [];
       }
     };
     fetchData();
   }, [elementIds]);
-
   return playerHistories;
 }
+
 // Path: src/usePlayerHistories.js
